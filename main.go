a990376354f31c9d212012ac5600d9a843982808
Harvey Coplestone <harveycoplestone@me.com>
2019-03-17 15:26:15 +0000

    updates

--- main.go
+++ main.go
@@ -2,26 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 	"sync"
 )
 
-func runSystem(seed int64, wg *sync.WaitGroup) {
+func runSystem(seed int64, wg *sync.WaitGroup, filename string) {
 	dla := NewDLASystem(480, 1.2, 1.7, 10000, seed)
 	// dla.DisplayGrid()
 	dla.isRunning = true
 
-	// f, err := os.OpenFile("data.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	f, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	i := 1
 	for {
 		// time.Sleep(1 * time.Millisecond)
 		dla.Update()
 
-		if i%100 == 0 {
-			fmt.Println(seed)
+		if i%10 == 0 {
+			fmt.Printf(".")
+			if _, err := f.Write([]byte(fmt.Sprintf("%d,%f\n", dla.numberOfParticles, dla.clusterRadius))); err != nil {
+				log.Fatal(err)
+			}
 		}
 
 		// if i%1000 == 0 {
@@ -29,21 +36,18 @@ func runSystem(seed int64, wg *sync.WaitGroup) {
 		// }
 		i++
 
-		// if _, err := f.Write([]byte(fmt.Sprintf("%d,%f\n", dla.numberOfParticles, dla.clusterRadius))); err != nil {
-		// 	log.Fatal(err)
-		// }
-
 		if dla.isRunning == false {
 			break
 		}
 	}
 
 	fmt.Println("\nFinished!")
-	wg.Done()
 
-	// if err := f.Close(); err != nil {
-	// 	log.Fatal(err)
-	// }
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+	wg.Done()
 
 	// dla.DisplayGrid()
 }
@@ -51,11 +55,18 @@ func runSystem(seed int64, wg *sync.WaitGroup) {
 func main() {
 	var wg sync.WaitGroup
 
-	wg.Add(1)
-	go runSystem(123, &wg)
+	var filenameComponents []string
+	var filename string
 
-	wg.Add(1)
-	go runSystem(789, &wg)
+	i := 0
+	for i < 100 {
+		filenameComponents = []string{"results/ensemble", strconv.Itoa(i), ".csv"}
+		wg.Add(1)
+		fmt.Printf("Starting system %d\n", i)
+		filename = strings.Join(filenameComponents, "")
+		go runSystem(int64(i), &wg, filename)
+		i++
+	}
 
 	wg.Wait()
 }
